2f88fdd3ceae8deefeaf18e105f9a2b22bea11c8
Zack338 <zachary.rouch@student.swic.edu>
2025-10-03 02:08:49 -0500

    Update data code

---
+++ src/data.js
@@ -0,0 +1,37 @@
+const DATA = [
+  {
+    id: "CIS277",
+    name: "Web Development",
+    students: [
+      {
+        id: 1,
+        name: "Maria",
+        assignments: [
+          { name: "Project 1", points: 85, maxPoints: 100 },
+          { name: "Quiz 1", points: 18, maxPoints: 20 },
+          { name: "Homework 1", points: 9, maxPoints: 10 },
+        ],
+      },
+      {
+        id: 2,
+        name: "John",
+        assignments: [
+          { name: "Project 1", points: 92, maxPoints: 100 },
+          { name: "Quiz 1", points: 20, maxPoints: 20 },
+          { name: "Homework 1", points: 10, maxPoints: 10 },
+        ],
+      },
+      {
+        id: 3,
+        name: "Alex",
+        assignments: [
+          { name: "Project 1", points: 75, maxPoints: 100 },
+          { name: "Quiz 1", points: 15, maxPoints: 20 },
+          { name: "Homework 1", points: 8, maxPoints: 10 },
+        ],
+      },
+    ],
+  },
+];
+
+export default DATA;
